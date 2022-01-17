e88f9bb867b96182c6939ab7c6aeb572c97f1095
Ibrat <ibrat@abutech.uz>
2022-01-17 12:44:18 +0500

    feat: implement validators

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
@@ -5,6 +6,14 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
 
   const config = new DocumentBuilder()
     .setTitle("NEST CRM ")
--- src/modules/groups/dto.ts
+++ src/modules/groups/dto.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
 
 export class GroupDTO {
+  @IsNumber()
   @ApiProperty({ type: Number })
   id: number;
+
+  @IsString()
   @ApiProperty({ type: String })
   name: string;
 }
