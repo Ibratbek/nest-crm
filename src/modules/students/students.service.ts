1ae727e2810e62c2b1be0d73559b7d28f68201d5
shaxboz <shaxboz@abutech.uz>
2022-01-21 09:22:15 +0500

    fix: merge conflict

--- src/modules/students/students.service.ts
+++ src/modules/students/students.service.ts
@@ -11,30 +11,11 @@ export class StudentsService {
     private readonly studentRepository: Repository<Student>
   ) {}
 
-<<<<<<< HEAD
   async getStudents(): Promise<Student[]> {
     return await this.studentRepository
       .createQueryBuilder("student")
       .leftJoinAndSelect("student.group", "group")
       .getMany();
-=======
-  async insertStudent(body: CreateStudentDTO): Promise<Student> {
-    const student = this.studentRepository.create({
-      first_name: body.firstName,
-      last_name: body.lastName,
-      group: { id: body.groupId },
-    });
-    return await this.studentRepository.save(student);
-  }
-
-  getStudents(): Observable<Student[]> {
-    return from(
-      this.studentRepository
-        .createQueryBuilder("student")
-        .leftJoinAndSelect("student.group", "group")
-        .getMany()
-    );
->>>>>>> 6f7b4e2743cbd83f9fe1620dc893155f81d80cc4
   }
 
   async getStudent(id: number): Promise<Student> {
@@ -45,7 +26,6 @@ export class StudentsService {
       .getOne();
   }
 
-<<<<<<< HEAD
   async insertStudent(body: CreateStudentDTO): Promise<Student> {
     const student = this.studentRepository.create({
       first_name: body.firstName,
@@ -59,9 +39,6 @@ export class StudentsService {
     body: UpdateStudentDTO,
     id: number
   ): Promise<UpdateResult> {
-=======
-  updateStudent(body: UpdateStudentDTO, id: number): Observable<UpdateResult> {
->>>>>>> 6f7b4e2743cbd83f9fe1620dc893155f81d80cc4
     const student = {
       first_name: body.firstName,
       last_name: body.lastName,
