f2f9a5497ddff0beb20556879bf4bd2d9ff533a4
Ibrat <73630955+Ibratbek@users.noreply.github.com>
2022-10-21 09:46:12 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+//
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
